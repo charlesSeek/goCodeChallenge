b80183887b885ea99dd11b31e6dc11007914a3ce
Charles Cui <ccui@l5188.modem>
2022-03-01 06:50:16 +1100

    add new rpc GetRaceById

--- racing/service/racing.go
+++ racing/service/racing.go
@@ -9,6 +9,9 @@ import (
 type Racing interface {
 	// ListRaces will return a collection of races.
 	ListRaces(ctx context.Context, in *racing.ListRacesRequest) (*racing.ListRacesResponse, error)
+
+	// GetRaceById will return a race by Id
+	GetRaceById(ctx context.Context, in *racing.GetRaceByIdRequest) (*racing.GetRaceByIdResponse, error)
 }
 
 // racingService implements the Racing interface.
@@ -28,4 +31,14 @@ func (s *racingService) ListRaces(ctx context.Context, in *racing.ListRacesReque
 	}
 
 	return &racing.ListRacesResponse{Races: races}, nil
+}
+
+func (s *racingService) GetRaceById(ctx context.Context, in *racing.GetRaceByIdRequest) (*racing.GetRaceByIdResponse, error) {
+	id := in.Id
+	race, err := s.racesRepo.GetRaceById(id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &racing.GetRaceByIdResponse{Race: race}, nil
 }
